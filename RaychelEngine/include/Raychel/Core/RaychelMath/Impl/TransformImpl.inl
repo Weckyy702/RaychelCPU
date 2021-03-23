7cbcf874c9053b61b85cc45c5f1ab34a930853ac
weckyy702 <weckyy702@gmail.com>
2021-03-23 23:07:26 +0100

    Started adding documentation and copyright notice

--- RaychelEngine/include/Raychel/Core/RaychelMath/Impl/TransformImpl.inl
+++ RaychelEngine/include/Raychel/Core/RaychelMath/Impl/TransformImpl.inl
@@ -1,3 +1,30 @@
+/**
+*\file TransformImpl.inl
+*\author weckyy702 (weckyy702@gmail.com)
+*\brief Implementation for Transforms
+*\date 2021-03-23
+*
+*MIT License
+*Copyright (c) [2021] [Weckyy702 (weckyy702@gmail.com | https://github.com/Weckyy702)]
+*Permission is hereby granted, free of charge, to any person obtaining a copy
+*of this software and associated documentation files (the "Software"), to deal
+*in the Software without restriction, including without limitation the rights
+*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*copies of the Software, and to permit persons to whom the Software is
+*furnished to do so, subject to the following conditions:
+*
+*The above copyright notice and this permission notice shall be included in all
+*copies or substantial portions of the Software.
+*
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*SOFTWARE.
+*
+*/
 #include "../Transform.h"
 
 namespace Raychel {
@@ -6,6 +33,15 @@ namespace Raychel {
     auto TransformImp<T>::apply(const vec3& p) const -> vec3
     {
         return (position - p) * rotation;
+    }
+
+    template<typename T>
+    template<typename To>
+    constexpr TransformImp<To> TransformImp<T>::to() const noexcept
+    {
+        static_assert(std::is_convertible_v<T, To>, "Raychel::TransformImp<T>::to<To>() requires T to be convertible to To!");
+        
+        return { position.to<To>(), rotation.to<To>() };
     }
 
 }
--- RaychelEngine/include/Raychel/Core/RaychelMath/Transform.h
+++ RaychelEngine/include/Raychel/Core/RaychelMath/Transform.h
@@ -1,3 +1,30 @@
+/**
+*\file Transform.h
+*\author weckyy702 (weckyy702@gmail.com)
+*\brief Header file for Transforms
+*\date 2021-03-23
+*
+*MIT License
+*Copyright (c) [2021] [Weckyy702 (weckyy702@gmail.com | https://github.com/Weckyy702)]
+*Permission is hereby granted, free of charge, to any person obtaining a copy
+*of this software and associated documentation files (the "Software"), to deal
+*in the Software without restriction, including without limitation the rights
+*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*copies of the Software, and to permit persons to whom the Software is
+*furnished to do so, subject to the following conditions:
+*
+*The above copyright notice and this permission notice shall be included in all
+*copies or substantial portions of the Software.
+*
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*SOFTWARE.
+*
+*/
 #ifndef RAYCHEL_TRANSFORM_H
 #define RAYCHEL_TRANSFORM_H
 
@@ -10,6 +37,11 @@
 namespace Raychel
 {
     
+    /**
+    *\brief An (almost) mathematical transform (scale is missing)
+    *
+    *\tparam _number Type of the Transform. Must be arithmetic
+    */
     template<RAYCHEL_NUMBER _number>
     struct TransformImp
     {
@@ -40,6 +72,15 @@ namespace Raychel
         {}
 
         vec3 apply(const vec3&) const;
+
+        /**
+        *\brief Convert the transform to another transform of type To
+        *
+        *\tparam To Type of the converted Transform
+        *\return TransformImp<T> 
+        */
+        template<typename To>
+        constexpr TransformImp<To> to() const noexcept;
 
         vec3 position;
         Quaternion rotation;
