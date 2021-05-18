5b8d7c51c401da789f9e17fd30024823550628f7
weckyy702 <weckyy702@gmail.com>
2021-05-18 08:58:39 +0200

    added a little test to verify that colors still work as intended

---
+++ RaychelEngine/src/test/RaychelMath/color.cpp
@@ -0,0 +1,35 @@
+#define RAYCHEL_NO_LOGGER
+#include "Raychel/Core/RaychelMath/color.h"
+#include "Raychel/Core/RaychelMath/Impl/colorImpl.inl"
+
+using color = Raychel::colorImp<double>;
+using namespace Raychel;
+
+constexpr colorImp<float> get_color() noexcept
+{
+    color c{1, 0, 1};
+    c *= 2.0;
+    c *= color{1, 0.1, 2};
+    c += color{0, 1, 0};
+
+    if (c == color{2, 1, 4}) {
+        const auto b = brightness(c);
+        c.r = c.g = c.b = b;
+    } else {
+        const auto b = 12;
+        c.r *= b;
+    }
+
+    return c.to<float>();
+}
+
+int main(int /*unused*/, char** /*unused*/) {
+    //all of these would be ill-formed without constexpr colors :^)
+    constexpr auto c = get_color();
+    constexpr size_t l = brightness(c);
+    std::array<char, l> arr;
+
+    std::cout << c << ", " << arr.size() << '\n';
+
+    return 0;
+}
