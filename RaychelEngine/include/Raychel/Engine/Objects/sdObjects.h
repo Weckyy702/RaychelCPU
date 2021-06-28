68f20c16987aad0e72191839182dd9bb0275fb21
weckyy702 <weckyy702@gmail.com>
2021-06-28 23:19:22 +0200

    Added SdPlane class
    This patch adds the class Raychel::SdPlane that derives from
    Raychel::SdObject. It represents an infinitely thin plane in 3D

--- RaychelEngine/include/Raychel/Engine/Objects/sdObjects.h
+++ RaychelEngine/include/Raychel/Engine/Objects/sdObjects.h
@@ -5,16 +5,36 @@
 
 namespace Raychel {
 
-    struct SdSphere : public SdObject
+    class SdSphere : public SdObject
     {
-
+    public:
         SdSphere(ObjectData&& data, float _r) : SdObject{std::move(data)}, radius{_r}
         {}
 
-        RAYCHEL_EXPORT [[nodiscard]] float eval(const vec3& p) const override;
+        RAYCHEL_EXPORT [[nodiscard]] float eval(const vec3& p) const noexcept override
+        {
+            return dist(p, transform().position) - radius;
+        }
 
     private:
         float radius = 0;
+    };
+
+    class SdPlane : public SdObject
+    {
+    public:
+        SdPlane(ObjectData&& data, const vec3& normal) : SdObject{std::move(data)}, normal_{normalize(normal)}
+        {}
+
+        SdPlane(ObjectData&& data, const vec3& normal, float offset_along_normal)
+            : SdObject{std::move(data)}, normal_{normalize(normal)}, offset_along_normal_(offset_along_normal)
+        {}
+
+        RAYCHEL_EXPORT [[nodiscard]] float eval(const vec3& p) const noexcept override;
+
+    private:
+        normalized3 normal_;
+        float offset_along_normal_ = 0.0F;
     };
 
 } // namespace Raychel
